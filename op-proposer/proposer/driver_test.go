bbbcc60cebaa5d47f2fd14ded29b1514f8ea8f38
Joonkyo Kim <rootwarp@gmail.com>
2024-11-15 20:21:55 +0700

    wip

--- op-proposer/proposer/driver_test.go
+++ op-proposer/proposer/driver_test.go
@@ -179,3 +179,71 @@ func TestL2OutputSubmitter_OutputRetry(t *testing.T) {
 		})
 	}
 }
+
+func TestL2OutputManual(t *testing.T) {
+	// Create L2output manually.
+	// https://etherscan.io/tx/0xac6741b3bfbafe51d0743d09f9199945c6bcd29c167b40be33f16599d017ce49
+	// _outputRoot	bytes32	0x9533dec7d7f7e271ef2d8214bd0cc3c7c8bdadac19b260518990116bd1f976f8
+	// _l2BlockNumber	uint256	950400
+	// _l1Blockhash	bytes32	0xd6cb4e7a9f4587db690bb7c88b6ae63b43590eca4b9f7153d09c5c1984092b97
+	// _l1BlockNumber	uint256	21192745
+
+	rpcURL := "https://eth-mainnet.g.alchemy.com/v2/zM3gFWh2YsPX8N2QzS38Ymzb8tKU822V"
+	logger := log.New()
+	metricer := metrics.NewMetrics("proposer")
+
+	ctx := context.Background()
+	l2ooAddr := common.HexToAddress("0x9a8701CcD7F8D5C2862D71341266057eA4669Ae9")
+	disputeAddr := common.HexToAddress("0xCF12aEe7dB364781CFd9739B2EBeC4A833343bc4")
+	l1Client, err := dial.DialEthClientWithTimeout(ctx, time.Second*10, logger, rpcURL)
+	require.Nil(t, err)
+
+	dial.NewActiveL2RollupProvider(ctx, l1Client, logger, metricer)
+
+	/*
+	cliConfig := txmgr.CLIConfig{
+		L1RPCURL:                  rpcURL,
+		NumConfirmations:          1,
+		PrivateKey:                "",
+		FeeLimitMultiplier:        1.0,
+		FeeLimitThresholdGwei:     2,
+		SafeAbortNonceTooLowCount: 1,
+		MinBaseFeeGwei:            1,
+		MinTipCapGwei:             1,
+		ResubmissionTimeout:       time.Second * 10,
+		ReceiptQueryInterval:      time.Second * 10,
+		NetworkTimeout:            time.Second * 10,
+		TxSendTimeout:             time.Second * 10,
+		TxNotInMempoolTimeout:     time.Second * 10,
+	}
+	*/
+	cliConfig := txmgr.NewCLIConfig(rpcURL, txmgr.DefaultBatcherFlagValues)
+	cliConfig.PrivateKey = "92f2412992bba948f81c374149ddc4051b470d9b70a4fdd99a99cb90814295b9"
+	txManager, err := txmgr.NewSimpleTxManager("proposer", logger, metricer, cliConfig)
+	require.Nil(t, err)
+
+	driver := DriverSetup{
+		Log:  logger,
+		Metr: metrics.NoopMetrics,
+		Cfg: ProposerConfig{
+			PollInterval:           time.Second * 10,
+			NetworkTimeout:         time.Second * 10,
+			ProposalInterval:       time.Second * 10,
+			L2OutputOracleAddr:     &l2ooAddr,
+			DisputeGameFactoryAddr: &disputeAddr,
+			DisputeGameType:        0,
+			AllowNonFinalized:      false,
+			WaitNodeSync:           false,
+		},
+		Txmgr:          txManager,
+		L1Client:       l1Client,
+		Multicaller:    nil,
+		RollupProvider: nil,
+	}
+	submitter, err := NewL2OutputSubmitter(driver)
+	require.Nil(t, err)
+
+	resp, _, err := submitter.FetchL2OOOutput(ctx)
+	_ = resp
+	require.Nil(t, err)
+}
